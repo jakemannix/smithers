9e0c3ddbe5a4d70adb7e798868e6b18d8e94c0b8
Jake Mannix <jake.mannix@gmail.com>
2023-06-25 23:35:31 -0700

    initial investigations with langchain and motorhead and tools

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Your loyal assistant',
+    author='YetAnotherUseless.com',
+    license='MIT',
+)
