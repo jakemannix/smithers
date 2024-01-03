ad24b15a39092e76d8cb1825079ed160d5c1e0e4
Liam Thompson <liam.thompson@iointel.com>
2024-01-02 16:38:38 -0800

    adding looping functionality for smithers via cli

--- src/main.py
+++ src/main.py
@@ -8,12 +8,22 @@ def build_and_run_search_agent(query: str):
     agent.run(query)
 
 
+def smithers_loop():
+    line = ''
+    while True:
+        print('What would you like to ask?')
+        line = input()
+        if line == 'exit'.strip().casefold():
+            break
+        build_and_run_search_agent(line)
+
+
 def search_cli():
     parser = argparse.ArgumentParser(
         prog='Smithers AI',
         description='This is a description of our cool AI'
     )
-    parser.add_argument('search', type=str,
-                        help='something you would like to look up')
-    args = parser.parse_args()
-    build_and_run_search_agent(args.search)
+    # parser.add_argument('search', type=str,
+    #                     help='something you would like to look up')
+    # args = parser.parse_args()
+    smithers_loop()
